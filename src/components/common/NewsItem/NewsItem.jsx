7217b3780d967790eb9c680c3d4b35ed33f3b205
Prajwal Bajracharya <bajracharya1994@gmail.com>
2020-11-05 21:22:18 +0545

    Modal WIP

--- src/components/common/NewsItem/NewsItem.jsx
+++ src/components/common/NewsItem/NewsItem.jsx
@@ -4,14 +4,20 @@ import Modal from 'react-modal';
 class NewsItem extends React.Component {
     state = {
         newsItem: [],
-        modalIsOpen: {}
+        modalIsOpen: false,
+        id: 0,
+        title: '',
+        url: '',
+        kids: []
     };
 
-    getModal(value) {
-        let modalKey = {};
-        modalKey[value] = true;
+    getModal(id, title, url, kids) {
         this.setState({
-            modalIsOpen: Object.assign( {}, this.state.modalIsOpen, modalKey)
+            modalIsOpen: true,
+            id,
+            title,
+            url,
+            kids
         } );
     }
 
@@ -44,16 +50,16 @@ class NewsItem extends React.Component {
             <ul className="news__list">
                 {this.state.newsItem.map((news) => (
                     <li className="news__item" key={news.id}>
-                        <a href='javascript:void(0);' className="news__link" onClick={() => this.getModal(news.id) }>
+                        <a href='javascript:void(0);' className="news__link" onClick={() => this.getModal(news.id, news.title, news.url, news.kids) }>
                             {news.title}
                         </a>
-                    <Modal isOpen={this.state.modalIsOpen[news.id]} onRequestClose={() => this.hideModal()}>
-                        <a href={news.url} target="_blank">{news.title}</a>
-                        <h6>Comment</h6>
-                        {news && news.kids && news.kids.map(comment => `<p>https://hacker-news.firebaseio.com/v0/item/${comment.id}.json?print=pretty</p>`)}
-                    </Modal>
                     </li>
                 ))}
+                <Modal isOpen={this.state.modalIsOpen[this.state.id]} onRequestClose={() => this.hideModal()}>
+                    <a href={this.state.url} target="_blank">{this.state.title}</a>
+                    <h6>Comment</h6>
+                    {this.state.kids && this.state.kids.map(comment => `<p>https://hacker-news.firebaseio.com/v0/item/${comment.id}.json?print=pretty</p>`)}
+                </Modal>
             </ul>
         );
     }
